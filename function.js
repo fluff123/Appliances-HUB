781f9c99fa763da69819cb44f4a9c8cbf78c3ee7
fluff123 <155054790+fluff123@users.noreply.github.com>
2024-01-06 22:21:31 +0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,82 @@
+
+function scrollToAbout() {
+    const aboutSection = document.getElementById('ABOUT');
+    const windowHeight = window.innerHeight;
+    const sectionHeight = aboutSection.offsetHeight;
+
+  
+    const scrollToY = aboutSection.offsetTop - (windowHeight - sectionHeight) / 2;
+
+    
+    window.scroll({ top: scrollToY, behavior: 'smooth' });
+}
+
+
+const aboutLink = document.querySelector('a[href="#About"]');
+aboutLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    scrollToAbout();
+});
+
+ function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    const windowHeight = window.innerHeight;
+    const sectionHeight = section.offsetHeight;
+
+    
+    const scrollToY = section.offsetTop - (windowHeight - sectionHeight) / 2;
+
+ 
+    window.scroll({ top: scrollToY, behavior: 'smooth' });
+}
+
+
+let prevScrollPos = window.pageYOffset;
+const header = document.querySelector('.header');
+
+window.onscroll = function () {
+    const currentScrollPos = window.pageYOffset;
+
+    if (prevScrollPos > currentScrollPos) {
+     
+        header.style.top = '0';
+    } else {
+     
+        header.style.top = `-${header.offsetHeight}px`;
+    }
+
+    prevScrollPos = currentScrollPos;
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    function scrollToSection(sectionId) {
+        const section = document.getElementById(sectionId);
+        if (section) {
+            section.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        }
+    }
+    
+    const aboutLink = document.querySelector('a[href="#About"]');
+    aboutLink.addEventListener('click', function (event) {
+        event.preventDefault(); 
+        scrollToSection('ABOUT');
+    });
+    
+    const productsLink = document.querySelector('a[href="#Products"]');
+    productsLink.addEventListener('click', function (event) {
+        event.preventDefault(); 
+        scrollToSection('Product');
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    const contactLink = document.querySelector('a[href="#Contact"]');
+    const contactSection = document.querySelector('#contact');
+
+    
+    contactLink.addEventListener("click", function (event) {
+        event.preventDefault(); 
+        contactSection.scrollIntoView({ behavior: 'smooth' });
+    });
+});
